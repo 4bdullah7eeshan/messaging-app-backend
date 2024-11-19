ed890f35d245f3b171eb64ea0ef37e2850313181
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-19 19:46:55 +0530

    feat: start the express app
    Setup the basic express app.

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`Messaging App running on port ${PORT}!`)
+);
