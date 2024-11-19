f8e24f18e9a34b6d230368d2d05a32b3ab795eb7
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-19 19:58:44 +0530

    wip: setup config file for express sess with prima

---
+++ config/session.js
@@ -0,0 +1,22 @@
+const expressSession = require("express-session");
+import { PrismaSessionStore } from '@quixo3/prisma-session-store';
+import { PrismaClient } from '@prisma/client';
+
+const sessionConfig = expressSession({
+    cookie: {
+        maxAge: 7 * 24 * 60 * 60 * 1000 // ms
+    },
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(
+        new PrismaClient(),
+        {
+            checkPeriod: 2 * 60 * 1000,  //ms
+            dbRecordIdIsSessionId: true,
+            dbRecordIdFunction: undefined,
+        }
+    )
+});
+
+module.exports = sessionConfig;
