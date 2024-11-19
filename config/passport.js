d3b5906d1c4460935666dd052a5bfb5348d785e7
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-20 01:40:42 +0530

    wip: setup config file for passport
    Placed LocalStrategy & JWTStrategy

---
+++ config/passport.js
@@ -0,0 +1,58 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const JwtStrategy = require("passport-jwt").Strategy;
+const { ExtractJwt } = require("passport-jwt");
+const bcrypt = require("bcryptjs");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+const jwtSecret = process.env.JWT_SECRET;
+
+module.exports = (passport) => {
+    passport.use(
+        new LocalStrategy(async (username, password, done) => {
+            try {
+                const user = await prisma.user.findUnique({ where: { username } });
+                if (!user) return done(null, false, { message: "User not found" });
+
+                const isValid = await bcrypt.compare(password, user.password);
+                if (!isValid) return done(null, false, { message: "Incorrect password" });
+
+                return done(null, user);
+            } catch (error) {
+                return done(error);
+            }
+        })
+    );
+
+    passport.use(
+        new JwtStrategy(
+            {
+                jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+                secretOrKey: jwtSecret,
+            },
+            async (jwtPayload, done) => {
+                try {
+                    const user = await prisma.user.findUnique({ where: { id: jwtPayload.userId } });
+                    if (user) return done(null, user);
+                    else return done(null, false);
+                } catch (error) {
+                    return done(error, false);
+                }
+            }
+        )
+    );
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const user = await prisma.user.findUnique({ where: { id } });
+            done(null, user);
+        } catch (error) {
+            done(error);
+        }
+    });
+};
