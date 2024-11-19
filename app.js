ff12bbf2b7888250f4c27a13eb9d377a0f07b107
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-20 01:45:17 +0530

    feat: initialize passport in the express app

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const passport = require("passport");
+
 
 const prismaSession = require("./config/session");
+require("./config/passport")(passport);
+
 
 const app = express();
 
@@ -9,6 +13,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use(prismaSession);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 const PORT = process.env.PORT || 3000;
