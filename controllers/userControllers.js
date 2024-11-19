1c231e3ca3681809152df141613e07b7b9eaa367
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-20 02:13:38 +0530

    feat: write logic to create/sign up a new user
    - Get user's email, username, and password.
    - Check if the user has input all the above fields.
    - Check if the user already exists in the db based on the email.
    - Hash the user's password with bcrypt
    - Create a new user with Prisma using the email, username, and the hashed password.

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -8,8 +8,36 @@ const jwtSecret = process.env.JWT_SECRET;
 const jwtExpiry = "1h";
 
 const createUser = asyncHandler(async (req, res) => {
-    
+    const { username, email, password } = req.body; // displayName entity in the User model was optional. Not needed to create an account.
+
+    // Check if all required fields are provided
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("All fields are required");
+    }
+
+    // Check if the user already exists
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create the user in db with Prisma
+    const user = await prisma.user.create({
+        data: {
+            username,
+            email,
+            password: hashedPassword,
+        },
+    });
+
+    res.status(201).json({ message: "User created successfully", user });
 });
+
 
 const loginUser = asyncHandler(async (req, res) => {
 
