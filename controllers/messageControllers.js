ddf6dd954e3ed7cc4218d8bc38cdc36ac072f45e
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-21 04:02:30 +0530

    refactor: consolidate controllers functions
    Planning to merge common functionalities.

--- controllers/messageControllers.js
+++ controllers/messageControllers.js
@@ -3,51 +3,22 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-const createPrivateMessage = asyncHandler(async (req, res) => {
+const createMessage = asyncHandler(async (req, res) => {
     
 });
 
-const sendPrivateMessage = asyncHandler(async (req, res) => {
+const getMessages = asyncHandler(async (req, res) => {
     
 });
 
-const createGroupMessage = asyncHandler(async (req, res) => {
-
+const getMessage = asyncHandler(async (req, res) => {
+    
 });
 
-const sendGroupMessage = asyncHandler(async (req, res) => {
-
+const updateMessage = asyncHandler(async (req, res) => {
+    
 });
 
-const getPrivateMessages = asyncHandler(async (req, res) => {
-
-});
-
-const getGroupMessages = asyncHandler(async (req, res) => {
-
-});
-
-const getPrivateMessage = asyncHandler(async (req, res) => {
-
-});
-
-
-const getGroupMessage = asyncHandler(async (req, res) => {
-
-});
-
-const updatePrivateMessage = asyncHandler(async (req, res) => {
-
-});
-
-const updateGroupMessage = asyncHandler(async (req, res) => {
-
-});
-
-const deletePrivateMessage = asyncHandler(async (req, res) => {
-
-});
-
-const deleteGroupMessage = asyncHandler(async (req, res) => {
-
+const deleteMessage = asyncHandler(async (req, res) => {
+    
 });
