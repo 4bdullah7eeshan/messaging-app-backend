fd840eb5ce3326132611b1b79eaa360dcd6e0ddf
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-21 00:44:54 +0530

    refactor: place handleValidationErrors at the end

--- validations/groupValidations.js
+++ validations/groupValidations.js
@@ -15,10 +15,12 @@ const handleValidationErrors = require("../middlewares/handleValidationErrors");
 const validateCreateGroup = [
     notEmpty("name", "Group name is required"),
     hasLength("name", 3, 30, "Group name must be between 3 and 30 characters"),
+    handleValidationErrors,
 ];
 
 const validateGetGroup = [
     isIntegerValue("id"),
+    handleValidationErrors,
 ]
 
 module.exports = {
