dc7069f3f49c6de65d6e87c566d874c38e6568d5
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-21 02:00:02 +0530

    refactor: refine validation errors json structure

--- middlewares/handleValidationErrors.js
+++ middlewares/handleValidationErrors.js
@@ -5,8 +5,12 @@ const handleValidationErrors = (req, res, next) => {
 
     if (!errors.isEmpty()) {
         return res.status(400).json({
+            status: "error",
             message: "Validation errors occurred",
-            errors: errors.array(),
+            errors: errors.array().map((error) => ({
+                field: error.param,
+                message: error.msg,
+            })),
         });
     }
     
