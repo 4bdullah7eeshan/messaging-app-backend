f55f8a95c1b83d8075de5c6a858d2c519d96a422
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-21 04:10:34 +0530

    feat: write controller logic for sending message
    Handles sending messages for both private and group situations.

--- controllers/messageControllers.js
+++ controllers/messageControllers.js
@@ -4,7 +4,24 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const createMessage = asyncHandler(async (req, res) => {
-    
+    const { content, targetId, isGroup } = req.body;  // Determine if it's a private or group message from frontend
+    const userId = req.user.id;
+
+    // Setup message data
+    const messageData = {
+        content,
+        senderId: userId,
+    };
+
+    if (isGroup) {
+        messageData.groupId = targetId;  // Add groupId entity for group message
+    } else {
+        messageData.receiverId = targetId;  // Add receiverId entity for private message
+    }
+
+    const message = await prisma.message.create({ data: messageData });
+
+    res.status(201).json({ message: "Message created successfully", message });
 });
 
 const getMessages = asyncHandler(async (req, res) => {
