78301c18a21e32b763a9f9ffb0ed4117d1a23c7e
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-20 17:22:17 +0530

    feat: create helper func to search a record in db
    Returns true if the record exists and false otherwise.

---
+++ helpers/prismaHelpers.js
@@ -0,0 +1,6 @@
+const prisma = require("prisma");
+
+const recordExists = async (model, field, value) => {
+    const record = await prisma[model].findUnique({ where: { [field]: value } });
+    return !!record;
+};
