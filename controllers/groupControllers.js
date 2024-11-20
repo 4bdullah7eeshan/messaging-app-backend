4b3587a60385c0f0aa33550b8e8e5b378b51b5a5
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-21 00:32:06 +0530

    feat: write logic for creating a new group in db
    - At the moment, the group is created with just its name. We can choose to place other info too at the time of creation.
    - No condition for unique names for groups.
    - At the time of group creation, the creator of the group becomes the admin and the first member of the group.

--- controllers/groupControllers.js
+++ controllers/groupControllers.js
@@ -4,30 +4,45 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const createGroup = asyncHandler(async (req, res) => {
-    
+    const { name } = req.body;
+    const userId = req.user.id;  // Need this to make the user who created the group, an admin and first member
+
+    const group = await prisma.group.create({
+        data: {
+            name,
+            adminId: userId,  // Set the creator as the admin
+            members: {
+                connect: { id: userId },  // Add the creator as the first member
+            },
+        },
+    });
+    // Think if you want to allow user to put all group info like description/DP at the time of creation
+
+    res.status(201).json({ message: "Group created successfully", group});
+
 });
 
 
 const getGroup = asyncHandler(async (req, res) => {
-    
+
 });
 
 
 const updateGroup = asyncHandler(async (req, res) => {
-    
+
 });
 
 
 const deleteGroup = asyncHandler(async (req, res) => {
-    
+
 });
 
 const getAllUsersGroups = asyncHandler(async (req, res) => {
-    
+
 });
 
 const getAllGroupMembers = asyncHandler(async (req, res) => {
-    
+
 });
 
 
@@ -46,3 +61,8 @@ const joinGroup = asyncHandler(async (req, res) => {
 const exitGroup = asyncHandler(async (req, res) => {
 
 });
+
+module.exports = {
+    createGroup,
+    
+}
