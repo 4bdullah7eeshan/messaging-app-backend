d81ebf9ce00668ff8c106b0c98124c883b2ce1f9
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-27 18:18:49 +0530

    feat: setup user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,14 +2,20 @@ const { Router } = require("express");
 const authenticateJwt = require("../middlewares/authenticateJWT");
 const userControllers = require("../controllers/userControllers");
 const groupControllers = require("../controllers/groupControllers");
+const friendsControllers = require("../controllers/friendControllers");
 
 
 const userRouter = Router();
 const isAuthenticated = authenticateJwt();
 
-userRouter.get("/messages", isAuthenticated ,userControllers.getMessages);
+userRouter.get("/", userControllers.getAllUsers);
+userRouter.get("/:id", userControllers.getUser);
+userRouter.patch("/:id", userControllers.updateUser);
+userRouter.delete("/:id", userControllers.deleteUser);
+userRouter.get("/messages", isAuthenticated , userControllers.getMessages);
 userRouter.get("/groups", isAuthenticated, groupControllers.getAllUsersGroups);
-userRouter.get("/friends", )
+userRouter.get("/friends", isAuthenticated, friendsControllers.getAllFriends);
+userRouter.get("/friend-requests", isAuthenticated, friendsControllers.getAllFriendRequests);
 
 
 module.exports = userRouter;
