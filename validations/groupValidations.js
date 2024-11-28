b9bd3c661e6b373d8f562cb55e3763dc8d2f4fbf
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-28 16:20:32 +0530

    feat: setup group validations for update & delete

--- validations/groupValidations.js
+++ validations/groupValidations.js
@@ -15,8 +15,24 @@ const handleValidationErrors = require("../middlewares/handleValidationErrors");
 const validateCreateGroup = [
     notEmpty("name", "Group name is required"),
     hasLength("name", 3, 30, "Group name must be between 3 and 30 characters"),
+    optionalWrapper(isValidUrl("avatarUrl", "Invalid group avatar URL")),
+    optionalWrapper(hasLength("description", 0, 200, "Description must be up to 200 characters")),
     handleValidationErrors,
 ];
+
+const validateUpdateGroup = [
+    optionalWrapper(isString("name", "Group name must be a string")),
+    optionalWrapper(hasLength("name", 3, 50, "Group name must be between 3 and 50 characters")),
+    optionalWrapper(isValidUrl("avatarUrl", "Invalid group avatar URL")),
+    optionalWrapper(hasLength("description", 0, 200, "Description must be up to 200 characters")),
+    handleValidationErrors,
+];
+
+const validateDeleteGroup = [
+    isIntegerValue("id", "Group ID must be a valid integer"),
+    handleValidationErrors,
+];
+
 
 const validateGetGroup = [
     isIntegerValue("id"),
@@ -25,6 +41,8 @@ const validateGetGroup = [
 
 module.exports = {
     validateCreateGroup,
+    validateUpdateGroup,
+    validateDeleteGroup,
     validateGetGroup,
 
 }
